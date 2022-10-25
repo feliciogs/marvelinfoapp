8223c4ee97353fd83e2ac10e55d4497d47b81c71
Felicio Gabriel <feliciogs@hotmail.com>
2022-10-24 22:39:24 -0300

    Configurando navigation

--- app/src/main/java/fegssp/app/io/marvelinfoapp/MainActivity.kt
+++ app/src/main/java/fegssp/app/io/marvelinfoapp/MainActivity.kt
@@ -2,15 +2,31 @@ package fegssp.app.io.marvelinfoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import fegssp.app.io.marvelinfoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding){
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {
+
+            }
+        }
     }
 }
